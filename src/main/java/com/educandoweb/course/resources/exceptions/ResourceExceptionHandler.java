7bcbb7d0422426d71e91f4719c8b0c6a1d60a918
Allyson Dibai <allysondibai@outlook.com>
2024-09-23 10:22:42 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -23,7 +23,7 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
-	@ExceptionHandler(DatabaseException.class)
+	@ExceptionHandler(DatabaseException.class) 
 	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
 		String error = "Database error";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
